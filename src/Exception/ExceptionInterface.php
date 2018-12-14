4d0b8a182b36d0fb707d81ef119bdbfd1cdc49c0
AppsOnline <42091234+apps-online@users.noreply.github.com>
2018-12-14 08:39:45 +0100

    Create ExceptionInterface.php
    Creeate ExceptionInterface.php

---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @name ExceptionInterface
+ * @version 1.0.0
+ * @author Miroslaw Kukuryka
+ * @copyright (c) 2018 (https://www.appsonline.eu)
+ * @link https://www.appsonline.eu
+ */
+namespace Xento\Exception;
+
+interface ExceptionInterface
+{
+} 
